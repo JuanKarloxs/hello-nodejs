c09a3929697882aa1761424b6b6e942706b04a7d
JuanKarloxs <111827757+JuanKarloxs@users.noreply.github.com>
2022-10-30 12:31:23 -0500

    COMMIT 2

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const http = require('http');
 
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World mother f*ck')
-});
+const server = http.createServer( (req, res) =>{
+    res.writeHead( 200, { 'Content-type': 'text/html;charset=uft-8' } );
+    res.write('<h3> Mi primer server Online de AWS </h3>');
+    res.end('Hello World mother f*ck');
+} );
 
-server.listen(port, () => {
+server.listen(port);
 console.log(`Server runing on port: ${port}`);
-});
