65b0850a2a3e7050a090e07ad2593684727d4aac
Juan Ramirez <juankarloxs36@gmail.com>
2022-10-30 12:22:30 -0500

    commit1

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World mother f*ck')
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World mother f*ck')
 });
 
 server.listen(port, () => {
